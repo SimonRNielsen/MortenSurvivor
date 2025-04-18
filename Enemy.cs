55f1f777466832822d9d7755cc3f67b8bdab73ac
SimonRNielsen <simonrn@gmail.com>
2025-04-18 22:35:01 +0200

    Added classes, sprites, sounds and music assets to folders

---
+++ Enemy.cs
@@ -0,0 +1,13 @@
+﻿using MortenSurvivor.Commands.States;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MortenSurvivor
+{
+    public class Enemy : Character, IState<Enemy>
+    {
+    }
+}
---
+++ Enums.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MortenSurvivor
+{
+
+
+    public enum UpgradeType
+    {
+
+        Mitre,
+        Rosary,
+        Bible,
+        WallGoose,
+        PopeStaff,
+        GeasterEgg
+
+    }
+
+
+    public enum ItemType
+    {
+
+        XPCrystal,
+        DamageBoost,
+        SpeedBoost,
+        HealBoost,
+        ConfuseEnemy,
+        ScareEnemy
+
+    }
+
+
+    public enum PlayerType
+    {
+
+        UndercoverMorten
+
+    }
+
+
+    public enum ProjectileType
+    {
+
+        Eggs,
+        GeasterEgg,
+        Magic
+
+    }
+
+
+    public enum EnvironmentTile
+    {
+
+        TopLeft,
+        Top,
+        TopRight,
+        Left,
+        Center,
+        Right,
+        BottomLeft,
+        Bottom,
+        BottomRight
+
+    }
+
+
+    public enum WeaponType
+    {
+
+        Sling
+
+    }
+
+
+    public enum EnemyType
+    {
+
+        Slow,
+        SlowChampion,
+        Fast,
+        FastChampion,
+        Goosifer
+
+    }
+
+}
