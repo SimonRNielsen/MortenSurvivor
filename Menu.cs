68d91064c23fa36bb858c64537fb0cc59cd70a6e
Simon Nielsen <simonrn@gmail.com>
2025-04-24 14:57:13 +0200

    TBC

--- Menu.cs
+++ Menu.cs
@@ -24,6 +24,7 @@ namespace MortenSurvivor
         private Texture2D sprite;
         private bool isActive = false;
         private bool isButton = false;
+        private bool isUpgrade = false;
         private bool hasMouseOver = false;
         private MenuItem type;
         private Vector2 position;
@@ -33,6 +34,10 @@ namespace MortenSurvivor
         private float rotation = 0f;
         private float layer = 0.9f;
         private Action action;
+        private List<Menu> relatedButtons;
+        private Menu parent;
+
+        public static Menu[] selectableUpgrades = new Menu[3];
 
         #endregion
         #region Properties
@@ -63,7 +68,7 @@ namespace MortenSurvivor
         #region Constructor
 
 
-        public Menu(MenuItem type, Action action)
+        public Menu(MenuItem type)
         {
 
             sprite = GameWorld.Instance.Sprites[type][0];
@@ -74,26 +79,43 @@ namespace MortenSurvivor
             {
                 case MenuItem.Start:
                     isActive = true;
-
+                    relatedButtons = new List<Menu>();
                     break;
                 case MenuItem.Pause:
-
+                    relatedButtons = new List<Menu>();
                     break;
                 case MenuItem.Loss:
-
+                    relatedButtons = new List<Menu>();
                     break;
                 case MenuItem.Upgrade:
-
+                    relatedButtons = new List<Menu>();
+                    isUpgrade = true;
                     break;
+            }
+
+        }
+
+
+        public Menu(MenuItem type, Action action, bool upgrade)
+        {
+
+            sprite = GameWorld.Instance.Sprites[type][0];
+            origin = new Vector2(sprite.Width / 2, sprite.Height / 2);
+            this.type = type;
+
+            switch (type)
+            {
                 case MenuItem.StackableButton:
                     isButton = true;
                     layer += 0.1f;
                     this.action = action;
+                    isUpgrade = upgrade;
                     break;
                 case MenuItem.SingleButton:
                     isButton = true;
                     layer += 0.1f;
                     this.action = action;
+                    isUpgrade = upgrade;
                     break;
             }
 
@@ -105,6 +127,14 @@ namespace MortenSurvivor
 
         public void Update()
         {
+
+            if (isUpgrade && selectableUpgrades[0] == null)
+            {
+                for (int i = 0; i < selectableUpgrades.Length; i++)
+                {
+
+                }
+            }
 
             switch (type)
             {
@@ -143,6 +173,12 @@ namespace MortenSurvivor
 
             if (isButton)
                 action?.Invoke();
+
+            if (isUpgrade)
+            {
+                GameWorld.Instance.GameMenu.Remove(this);
+                selectableUpgrades = new Menu[3];
+            }
 
         }
 
